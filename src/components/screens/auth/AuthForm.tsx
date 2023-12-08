217479002904d4807bc2138f12f0294813083b12
alina-sadykova <asadykovaa@gmail.com>
2023-12-08 08:29:28 -0600

    auth validation

--- src/components/screens/auth/AuthForm.tsx
+++ src/components/screens/auth/AuthForm.tsx
@@ -10,87 +10,121 @@ import Box from "@mui/material/Box";
 import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { authSchema } from "./schema";
+import { LoginInput } from "@/contexts/AuthContext/types";
+import { useAuthContext } from "@/contexts/AuthContext";
 
 export default function AuthForm() {
-	const [isLogin, setIsLogin] = React.useState(true);
+  const [isLogin, setIsLogin] = React.useState(true);
+  const { login, register, user } = useAuthContext();
+  const { handleSubmit, control } = useForm<LoginInput>({
+    resolver: yupResolver(authSchema),
+    mode: "onChange",
+  });
 
-	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		const data = new FormData(event.currentTarget);
-		console.log({
-			email: data.get("email"),
-			password: data.get("password"),
-		});
-	};
+  function onSubmit(data: LoginInput) {
+    console.log(data);
+    if (isLogin) {
+      login(data);
+    } else {
+      register(data);
+    }
+  }
 
-	function handleChangeLoginType() {
-		setIsLogin((prev) => !prev);
-	}
+  function handleChangeLoginType() {
+    setIsLogin((prev) => !prev);
+  }
 
-	return (
-		<Container component="main" maxWidth="xs">
-			<CssBaseline />
-			<Box
-				sx={{
-					marginTop: 8,
-					display: "flex",
-					flexDirection: "column",
-					alignItems: "center",
-				}}
-			>
-				<Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
-					<LockOutlinedIcon />
-				</Avatar>
-				<Typography component="h1" variant="h5">
-					{isLogin ? "Sign in" : "Sign up"}
-				</Typography>
-				<Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-					<TextField
-						margin="normal"
-						required
-						fullWidth
-						id="email"
-						label="Email Address"
-						name="email"
-						autoComplete="email"
-						autoFocus
-					/>
-					<TextField
-						margin="normal"
-						required
-						fullWidth
-						name="password"
-						label="Password"
-						type="password"
-						id="password"
-						autoComplete="current-password"
-					/>
-					<Button
-						type="submit"
-						fullWidth
-						variant="contained"
-						sx={{ mt: 3, mb: 2 }}
-					>
-						{isLogin ? "Sign in" : "Sign up"}
-					</Button>
-					<Grid container>
-						<Grid item xs>
-							{isLogin && (
-								<Link href="#" variant="body2">
-									Forgot password?
-								</Link>
-							)}
-						</Grid>
-						<Grid item>
-							<Link onClick={handleChangeLoginType} href="#" variant="body2">
-								{isLogin
-									? "Don't have an account? Sign Up"
-									: "Already have an account? Sign In"}
-							</Link>
-						</Grid>
-					</Grid>
-				</Box>
-			</Box>
-		</Container>
-	);
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          {isLogin ? "Sign in" : "Sign up"}
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+          sx={{ mt: 1 }}
+        >
+          <Controller
+            name="email"
+            control={control}
+            render={({ field, fieldState: { error, invalid } }) => (
+              <TextField
+                {...field}
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                error={invalid}
+                // error={!!error} //error = {} -> true, error = undefined -> false
+                helperText={error?.message}
+              />
+            )}
+          />
+          <Controller
+            name="password"
+            control={control}
+            render={({ field, fieldState: { error, invalid } }) => (
+              <TextField
+                {...field}
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                error={invalid}
+                helperText={error?.message}
+              />
+            )}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            {isLogin ? "Sign in" : "Sign up"}
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              {isLogin && (
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              )}
+            </Grid>
+            <Grid item>
+              <Link onClick={handleChangeLoginType} href="#" variant="body2">
+                {isLogin
+                  ? "Don't have an account? Sign Up"
+                  : "Already have an account? Sign In"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </Container>
+  );
 }
--- src/contexts/AuthContext/index.tsx
+++ src/contexts/AuthContext/index.tsx
@@ -1,67 +1,73 @@
-import React, { FC, createContext, useState } from "react";
+"use client";
+import React, { FC, createContext, useContext, useState } from "react";
 import {
-	createUserWithEmailAndPassword,
-	signInWithEmailAndPassword,
-	signOut,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
 } from "firebase/auth";
 import { auth } from "@/firebase/firebase.config";
 import { User } from "@/models/user";
 import { ContextProps, LoginInput } from "./types";
 
 interface IAuthContext {
-	user: User | null;
+  user: User | null;
+  register: (input: LoginInput) => void;
+  login: (input: LoginInput) => void;
+  logout: () => void;
 }
 
 const authContext = createContext<IAuthContext>({} as IAuthContext);
 
-const AuthContext: FC<ContextProps> = ({ children }) => {
-	const [user, setUser] = useState<User | null>(null);
+export const useAuthContext = () => useContext(authContext);
 
-	async function register({ email, password }: LoginInput) {
-		try {
-			const userCredential = await createUserWithEmailAndPassword(
-				auth,
-				email,
-				password
-			);
+const AuthContextProvider: FC<ContextProps> = ({ children }) => {
+  const [user, setUser] = useState<User | null>(null);
 
-			setUser({ email: userCredential.user.email as string });
-			console.log(userCredential, "userCredential");
-		} catch (e) {
-			console.log(e);
-		}
-	}
+  async function register({ email, password }: LoginInput) {
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
 
-	async function login({ email, password }: LoginInput) {
-		try {
-			const userCredential = await signInWithEmailAndPassword(
-				auth,
-				email,
-				password
-			);
+      setUser({ email: userCredential.user.email as string });
+      console.log(userCredential, "userCredential");
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
-			setUser({ email: userCredential.user.email as string });
-			console.log(userCredential, "userCredential");
-		} catch (e) {
-			console.log(e);
-		}
-	}
+  async function login({ email, password }: LoginInput) {
+    try {
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
 
-	async function logout() {
-		try {
-			await signOut(auth);
-		} catch (e) {
-			console.log(e);
-		}
-	}
+      setUser({ email: userCredential.user.email as string });
+      console.log(userCredential, "userCredential");
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
-	const value = {
-		user,
-		register,
-		login,
-		logout,
-	};
-	return <authContext.Provider value={value}>{children}</authContext.Provider>;
+  async function logout() {
+    try {
+      await signOut(auth);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const value = {
+    user,
+    register,
+    login,
+    logout,
+  };
+  return <authContext.Provider value={value}>{children}</authContext.Provider>;
 };
 
-export default AuthContext;
+export default AuthContextProvider;
